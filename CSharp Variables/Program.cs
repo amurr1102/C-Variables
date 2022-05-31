ffc253b29d455eb9713ec71a2c2610c411aeb53f
aemurr1102 <aemurray1102@gmail.com>
2022-05-31 13:44:14 -0500

    initial work

---
+++ CSharp Variables/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharp_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         string waltFirstName = ("Walter");
+            char waltInitial = 'H';
+            string waltLastName = ("White");
+            decimal waltAge = 50.4m;
+         double waltHouseNum = 308;
+            var waltAddress = ("Negra Arroyo Lane");
+                int waltZip = 87014;
+            Console.WriteLine($"My name is {waltFirstName} {waltInitial}. {waltLastName}. I am {waltAge} years old. I live at {waltHouseNum} {waltAddress}, Albuquerque, New Mexico, {waltZip}");
+
+        }
+    }
+}
